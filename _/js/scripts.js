dc105d4a61f6b3bf9a0ea64fb2bd398c20438f1f
Mark <mark@digiwig.com>
2014-04-18 19:51:12 +0100

    Good but line height issues between text elements throwing gutter off.

--- _/js/scripts.js
+++ _/js/scripts.js
@@ -24,9 +24,39 @@ $(document).ready(function() {
 			});
 	}						   
 
-	/* RANDOM BACKGROUND COLOURS */	
+	/* RANDOM BACKGROUND COLOURS */
+	function ColorLuminance(hex, lum) {
+	
+		// validate hex string
+		hex = String(hex).replace(/[^0-9a-f]/gi, '');
+		if (hex.length < 6) {
+			hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+		}
+		lum = lum || 0;
+	
+		// convert to decimal and change luminosity
+		var rgb = "#", c, i;
+		for (i = 0; i < 3; i++) {
+			c = parseInt(hex.substr(i*2,2), 16);
+			c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+			rgb += ("00"+c).substr(c.length);
+		}
+	
+		return rgb;
+	}
+	
+	
+
+  
+	var colours = ['9c6e81','89ae95','5b4f66','c4c583','676bb5','269c6f','449fa3','8091d1','c18c7b','32596e','aba1a9','4794ac','34C7DD','d9b4be','6e909c','7a8f88','6e7e9c','b15b58','d0dc9a','ec8672','bcd3ea','c5c5c5','b5b5b5','b15883','a5a5a5','d5d5d5'];
+
 	setInterval(function() {
-		var colour = '#'+Math.floor(Math.random()*16777215).toString(16);
+		//var colour = '#'+Math.floor((Math.random()*16777215) + 127).toString(16);
+		//var colour = pastelColors();
+		var colour = '#'+colours[Math.floor(Math.random() * colours.length)];
+		$("p").css("color", ColorLuminance(colour, -0.5));
+		$("header").css("background-color", ColorLuminance(colour, -0.8));
+		$("a.active").css("color", ColorLuminance(colour, 0.5));
 		$(main).css("background-color", colour);
 	}, 15000);
 
--- index.php
+++ index.php
@@ -11,7 +11,8 @@ header("Content-type: text/html; charset=utf-8");
     <link rel="apple-touch-icon" href="apple-touch-icon.png" />
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 	<link rel='stylesheet' href='_/css/reset.css' type='text/css' media='all' />
-    <link rel='stylesheet' href='_/css/styles.css' type='text/css' media='all' />       
+    <link rel='stylesheet' href='_/css/styles.css' type='text/css' media='all' />
+	<link rel='stylesheet' href='_/css/pages.css' type='text/css' media='all' />           
 </head>
 <body id="home" ontouchstart="">
 	<header id="header">Freelance Web Developer</header>
@@ -26,10 +27,46 @@ header("Content-type: text/html; charset=utf-8");
         <a href="mailto:mark@digiwig.com"><img src="_/images/icons/mailto.png" width="15" height="15"/></a>
     </nav>
     <main id="main">
-    	<section class="home_top"></section>
-        <section class="home_services"></section>
-        <section class="home_about"></section>
-		<section class="home_blog"></section>        
+    	<section class="home_top">
+        	<h1>Mark Wigglesworth</h1>
+            <p><span>Freelance Web Developer &amp; Designer, London</span>HTML5 / CSS3 / RWD / jQuery / PHP<span>CodeIgniter / Wordpress / ExpressionEngine</span></p>
+            <a class="button">View Portfolio</a>
+            <img width="70%" height="auto" src="_/images/laptop.png"/>
+        </section>
+        <section class="home_services">
+        	<h2>Services</h2>
+            <p>The short and sweet of what I do,<br/>click through to read a little bit more...</p>
+            <figure>
+            	<a href="/services#Web" title="Web"><img width="auto" height="auto" src="_/images/web.png" alt="Web"/></a>
+            	<figcaption>Web</figcaption>
+            </figure>
+            <figure>
+            	<a href="/services#Support" title="Support"><img width="auto" height="auto" src="_/images/support.png" alt="Support"/></a>
+            	<figcaption>Support</figcaption>
+            </figure>
+            <figure>
+            	<a href="/services#SEO" title="SEO"><img width="auto" height="auto" src="_/images/seo.png" alt="SEO"/></a>
+            	<figcaption>SEO</figcaption>
+            </figure>
+            <figure>
+            	<a href="/services#Apps" title="Apps"><img width="auto" height="auto" src="_/images/applications.png" alt="Apps"/></a>
+            	<figcaption>Apps</figcaption>
+            </figure>
+            <br/>
+            <a class="button">More</a>
+        </section>
+        <section class="home_about">
+        	<h2>About Me</h2>
+            <p>I am 32 years old. London is my home.<br/>I work full time as a Freelance Web Developer and Designer</p>
+            <img src="_/images/photo.png" width="22%" height="auto" alt="About Me"/>
+            <a class="button" href="about-me">More</a>            
+        </section>
+		<section class="home_blog">
+            <h2>Latest Blog Action!</h2>            
+            <h3>Super Responsive Multi Level Navigation!</h3>
+            <p>23rd Oct 2013</p>
+            <a class="button">Read More</a>        
+        </section>        
         <footer>
             <a href="#"><img width="8%" height="auto" src="_/images/icons/email.png"/></a>      
             <a href="#"><img width="8%" height="auto" src="_/images/icons/facebook.png"/></a>
